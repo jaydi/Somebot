0ef08ff5d35c6bbc207a7e7a9ca21eb89f8250a2
Will Lee <jaydi727@gmail.com>
2016-05-19 17:26:59 +0900

    no message

--- db/migrate/20160512110052_create_users.rb
+++ db/migrate/20160512110052_create_users.rb
@@ -2,9 +2,9 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :account_id, index: true, null: false
-      t.column :user_key, 'BIGINT UNSIGNED', index: true, null: false
-      t.column :last_chat_key, 'BIGINT UNSIGNED', null: false
-      t.column :last_msg_id, 'BIGINT UNSIGNED', null: false
+      t.integer :user_key, limit: 8, index: true, null: false
+      t.integer :last_chat_key, limit: 8, null: false
+      t.integer :last_msg_id, limit: 8, null: false
       t.integer :status, index: true, null: false, default: 10
 
       t.timestamps null: false
